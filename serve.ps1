a4bd25fc8d800a01598ca6f7384a51cb10b83237
Nguyễn Xuân Sơn <nguyenson.0312vn@gmail.com>
2026-03-21 20:48:31 +0700

    Publish portfolio update 2026-03-21 13:48 UTC

--- serve.ps1
+++ serve.ps1
@@ -2,3 +2,4 @@ $ErrorActionPreference = 'Stop'
 $root = 'C:\Users\nguye\.openclaw\workspace\portfolio-site'
 Set-Location $root
 node .\server.js
+# Local editor: http://localhost:8933/local-admin.html
